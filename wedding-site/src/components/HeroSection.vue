7eb47408759fcf03a1e4edc206f2bfca41b52a44
Slaymish <hamishapps@gmail.com>
2023-12-07 16:50:33 +1300

    Got rid of resizing font

--- wedding-site/src/components/HeroSection.vue
+++ wedding-site/src/components/HeroSection.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid class="hero-section">
     <v-row class="fill-height" align="center" justify="start">
-      <v-col cols="12" sm="10" md="8" lg="6" class="text-box" :style="textStyle">
+      <v-col cols="12" sm="10" md="8" lg="6" class="text-box">
         <div class="hero-subtitle">Together with their families</div>
         <h1 class="display-1">{{ mainTitle }}</h1>
         <div class="hero-subtitle">Invite you to join them</div>
@@ -15,10 +15,6 @@
 <script lang="ts">
 import { defineComponent, onMounted, onUnmounted, ref, computed } from 'vue';
 import { RouterLink } from 'vue-router';
-
-const initialFontSize = 5;
-const minFontSize = 4;
-const shrinkSpeed = 300;
 
 export default defineComponent({
   name: 'HeroSection',
@@ -34,29 +30,6 @@ export default defineComponent({
       type: String,
       required: true
     }
-  },
-  setup() {
-    const baseFontSize = ref(initialFontSize);
-
-    const textStyle = computed(() => ({
-      fontSize: `${baseFontSize.value}rem`
-    }));
-
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
-      const newFontSize = Math.max(initialFontSize - scrollPosition / shrinkSpeed, minFontSize);
-      baseFontSize.value = newFontSize;
-    };
-
-    onMounted(() => {
-      window.addEventListener('scroll', handleScroll);
-    });
-
-    onUnmounted(() => {
-      window.removeEventListener('scroll', handleScroll);
-    });
-
-    return { baseFontSize, textStyle, handleScroll };
   }
 });
 </script>
@@ -67,32 +40,33 @@ export default defineComponent({
   padding-left: 10%; /* Or any other percentage or fixed padding you prefer */
   height: 100vh;
   min-height: 600px;
-  margin-bottom: 5rem;
-}
-
-.hero-subtitle {
-  font-size: 1.5rem;
-  margin-bottom: 0.5rem;
-}
-
-.display-1 {
-  margin-bottom: 0.5rem;
-}
-
-.display-2 {
-  margin-top: 0.5rem;
+  display: flex;
+  align-items: center;
 }
 
 .text-box {
-  margin-top: 10%;
-  transition: font-size 0.2s;
   color: var(--color-text);
   font-family: var(--font-title);
   text-align: left; /* Ensure text within the text box is left-aligned */
 }
 
+.display-1 {
+  font-size: 7rem;
+  margin-bottom: 0.5rem;
+}
+
+.display-2 {
+  font-size: 3rem;
+  margin-bottom: 0.5rem;
+}
+
+.hero-subtitle {
+  font-size: 1.5rem;
+  margin-bottom: 0.2rem;
+}
+
 .rsvp-button {
-  margin-top: 2rem;
+  margin-top: 1rem;
   text-transform: uppercase;
   transition: all 0.3s ease-in-out;
 }
