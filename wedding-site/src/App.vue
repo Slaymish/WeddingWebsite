38f533d9afa12eadadf69c266a7cf0fe2e0cfd34
Slaymish <hamishapps@gmail.com>
2023-12-07 16:50:56 +1300

    Removed transition between pages

--- wedding-site/src/App.vue
+++ wedding-site/src/App.vue
@@ -1,43 +1,18 @@
+<template>
+  <v-app>
+    <router-view />
+  </v-app>
+</template>
+
 <script lang="ts">
 import FooterComponent from './components/FooterComponent.vue'
-import NavBar from './components/NavBar.vue'
 
 export default {
   components: {
-    FooterComponent,
-    NavBar
+    FooterComponent
   }
 }
 </script>
 
-<template>
-  <router-view v-slot="{ Component }">
-    <transition name="fade" mode="out-in">
-      <component :is="Component" :key="$route.path" />
-    </transition>
-  </router-view>
-</template>
-
 <style scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.2s ease;
-}
-
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-}
-
-@media (min-width: 768px) {
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity 0.5s ease;
-  }
-
-  .fade-enter,
-  .fade-leave-to {
-    opacity: 0;
-  }
-}
 </style>
