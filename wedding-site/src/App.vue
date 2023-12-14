e20f0d08ab470dd0b43f4f6e05fec632558628c3
Hamish Burke <burkehami@myvuw.ac.nz>
2023-12-14 20:26:59 +1300

    Update background image in App.vue

--- wedding-site/src/App.vue
+++ wedding-site/src/App.vue
@@ -15,10 +15,11 @@ export default {
   font-family: var(--font-text);
   background-color: var(--color-background);
   color: var(--color-text);
-  background-image: url('/src/assets/images/flower_header.webp'); /* Ensure this is the correct path */
+  background-image: url('/src/assets/images/flower_header_tiled.png'); /* Ensure this is the correct path */
   background-repeat: repeat-x;
   background-size: auto 200px; /* Maintain the aspect ratio and fill the height */
-  background-position: top;
+  background-position: center top;  
+  min-height: 100vh;
   width: 100%;
   padding-top: 90px; /* Adjust this to the new size of your image */
 }
