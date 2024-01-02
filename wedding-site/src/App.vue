52f146179d2a6f4d01460ed5010212ec6a3338e0
Slaymish <hamishapps@gmail.com>
2024-01-02 22:34:44 +1300

    Refactor App.vue styling

--- wedding-site/src/App.vue
+++ wedding-site/src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <v-app class="main">
-    <router-view />
-  </v-app>
+  <div class="back">
+    <v-app class="main">
+      <router-view />
+    </v-app>
+  </div>
 </template>
 
 <script lang="ts">
@@ -13,14 +15,27 @@ export default {
 <style scoped>
 .main {
   font-family: var(--font-text);
-  background-color: var(--color-background);
+  background-color: var(--white-mute);
   color: var(--color-text);
+  min-height: 100vh;
+  max-width: 900px;
+  margin: 0 auto;
+  padding-top: 90px; /* Adjust this to the new size of your image */
+  box-shadow: 0px 0px 30px rgba(0, 0, 0, 0.2);
+}
+
+.back {
+  background-color: var(--color-background);
   background-image: url('/src/assets/images/flower_header_tiled.png'); /* Ensure this is the correct path */
   background-repeat: repeat-x;
-  background-size: auto 200px; /* Maintain the aspect ratio and fill the height */
-  background-position: center top;  
-  min-height: 100vh;
-  width: 100%;
-  padding-top: 90px; /* Adjust this to the new size of your image */
+  background-size: auto 350px; /* Maintain the aspect ratio and fill the height */
 }
+
+@media (max-width: 900px) {
+  .main {
+    padding-top: 0px;
+    background-color: var(--color-background);
+  }
+}
+
 </style>
