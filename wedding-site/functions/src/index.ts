833dd8548e2796b684fd26ca2ef9f0ef69191e89
Slaymish <hamishapps@gmail.com>
2024-01-10 22:58:00 +1300

    Add email notification to couple for new RSVP

--- wedding-site/functions/src/index.ts
+++ wedding-site/functions/src/index.ts
@@ -24,6 +24,21 @@ exports.sendEmailConfirmation = functions.firestore
       msg.text = `Dear ${newValue.name},\n\nWe're sorry to hear you can't make it. We'll miss you!`;
     }
 
+    // Send another email to the couple.
+    const msgToCouple = {
+      to: "hamishapps@gmail.com",
+      from: "me@hamishburke.dev",
+      subject: `New RSVP (${newValue.name})`,
+      text: `Dear Lani and Finlay,\n\n ${newValue.name} has RSVP'd to your wedding. They are ${newValue.attending ? "attending" : "not attending"} and have ${newValue.dietaryRequirements ? "dietary requirements" : "no dietary requirements"}.View all attending at https://laniandfinlay.com/attending.\n\nLove,\nHamish\n\nPS: This email was sent by a robot.`,
+    };
+
+    // Send the email.
+    sgMail.send(msgToCouple).then(() => {
+      console.log("Email sent");
+    }).catch((error) => {
+      console.error(error);
+    });
+
     return sgMail.send(msg).then(() => {
       console.log("Email sent");
     }).catch((error) => {
